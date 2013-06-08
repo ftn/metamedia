197c834d994396d3b1c14a9c519896b66fab6dd3
Víctor Terrón <vterron@iaa.es>
2013-06-08 21:36:41 +0200

    Make MetaMedia.get_axes() return a generator of named tuples

--- rss/metamedia.py
+++ rss/metamedia.py
@@ -3,6 +3,7 @@
 # Author: Victor Terron (c) 2013
 # License: GNU GPLv3
 
+import collections
 import json
 import requests
 
@@ -15,17 +16,21 @@ class MetaMedia(object):
         self.auth_data = {'user' : user, 'password' : password}
 
     def get_axes(self):
-        """ Return the axes as a list of dictionaries """
+        """ A generator that yields the axes as named tuples """
 
         data = 'json=' + json.dumps(self.auth_data)
         headers = {'Content-Type': 'application/json'}
         kwargs = dict(headers=headers, data=data)
         r = requests.get(self.AXES_URL, **kwargs)
-        return r.json()
+        axes = r.json()
+
+        Axis = collections.namedtuple('Axis', axes[0].keys())
+        for axis in axes:
+            yield Axis(*axis.values())
 
 if __name__ == "__main__":
 
     metamedia = MetaMedia('anonymous', 'qwerty')
     for axis in metamedia.get_axes():
-        print axis['name'], axis['left_term'], axis['right_term']
+        print axis.name, axis.left_term, axis.right_term
 
