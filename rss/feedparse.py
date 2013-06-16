e1ccdf56e2e703479e43d4b12b70f65f20ed5174
Víctor Terrón <vterron@iaa.es>
2013-06-16 13:36:01 +0200

    Add query_axis_position() module-level function

--- rss/feedparse.py
+++ rss/feedparse.py
@@ -51,6 +51,32 @@ def query_yes_no(question, default = True):
             print "Please respond with 'yes' or 'no' (or 'y' or 'n')"
             print
 
+def query_axis_position(axis_name, left_term, right_term):
+    """ Force the user to assign an axis a value in the range [-10, 10].
+
+    Show the user the name of an ideological axis (for example, 'Economics')
+    and the left and right extremes ('Laissez-faire' and 'Interventionism',
+    respectively) and force a numeric response in the range [-10, 10]. The
+    user's answer is converted to the [0, 20] range before being returned.
+
+    """
+
+    # Example: "Economics (Laissez-Faire / Interventionism): "
+    msg = "%s (%s / %s): " % (axis_name, left_term, right_term)
+
+    while True:
+        try:
+            position = int(raw_input(msg))
+            if not -10 <= position <= 10:
+                raise ValueError
+            else:
+                # [-10, 10] to [0, 20]
+                return position + 10
+
+        except ValueError:
+            print "Position along axes must be in range [-10, 10]"
+            print
+
 if __name__ == "__main__":
 
     url = sys.argv[1]
